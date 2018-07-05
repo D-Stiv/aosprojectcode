469af29b6a8f55e416ae4c46bc0c8b9910a95cf6
Steve <steveludovic.gounoue@mail.polimi.it>
2018-07-05 12:13:38 +0200

    added the necessary methods in CloudSim

--- src/org/cloudbus/cloudsim/core/CloudSim.java
+++ src/org/cloudbus/cloudsim/core/CloudSim.java
@@ -3,6 +3,7 @@ package org.cloudbus.cloudsim.core;
 import java.util.Calendar;
 
 public class CloudSim {
+	private static int scenarioNbr = 0;
 
 	public static double clock() {
 		// TODO Auto-generated method stub
@@ -10,18 +11,18 @@ public class CloudSim {
 	}
 
 	public static void startSimulation() {
-		// TODO Auto-generated method stub
+		scenarioNbr++;
+		System.out.println("Scenario number " + scenarioNbr);
 		
 	}
 
 	public static void terminateSimulation() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("This simulation terminates");
+
 	}
 
 	public static void init(int num_user, Calendar calendar, boolean trace_flag, double d) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("num_user = " + num_user + "\ncalendar = " + calendar.getTime() + "\nTrace flag = " + trace_flag);
 	}
 
 }
