d38538bb6afa1beea8f23218216702b37b0c2a1e
Steve <steveludovic.gounoue@mail.polimi.it>
2018-07-05 13:53:30 +0200

    Creation of class ExponentialDistribution

--- src/org/apache/commons/math3/distribution/ExponentialDistribution.java
+++ src/org/apache/commons/math3/distribution/ExponentialDistribution.java
@@ -1,14 +1,45 @@
 package org.apache.commons.math3.distribution;
 
 public class ExponentialDistribution {
+	private double lambda;
+	private double mean;
+	private double stdDev; // sigma which is sqrt(sigma^2)
 
 	public ExponentialDistribution(double d) {
-		// TODO Auto-generated constructor stub
+		setLambda(d);
+		setMean(d);
+		setStdDev(d);
+	}
+
+	private void setStdDev(double d) {
+		stdDev = mean;
+		
+	}
+
+	private void setMean(double d) {
+		mean = 1/lambda;
+		
+	}
+
+	public double getLambda() {
+		return lambda;
+	}
+
+	public double getMean() {
+		return mean;
+	}
+
+	public double getStdDev() {
+		return stdDev;
+	}
+
+	public void setLambda(double lambda) {
+		this.lambda = lambda;
 	}
 
 	public double sample() {
-		// TODO Auto-generated method stub
-		return 0;
+		double sample = 10 * (0.1 + Math.random());
+		return sample;
 	}
 
 }
